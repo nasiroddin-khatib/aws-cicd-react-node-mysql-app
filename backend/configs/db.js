58776dbc9499efedfabbf919cd30b7d69c37759d
nasiroddin <nasirkhatib01.com>
2026-05-20 11:23:13 +0000

    .

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const host = process.env.DB_HOST || 'dev-db-instance.cdbmlufgqkjd.ap-south-1.rds.amazonaws.com';
+const host = process.env.DB_HOST || 'dev-db-instance.cbakmw02gj1f.ap-south-1.rds.amazonaws.com';
 const port = process.env.DB_PORT || '3306';
-const user = process.env.DB_USER || 'appuser';
-const password = process.env.DB_PASSWORD || 'learnIT02#';
-const database = process.env.DB_NAME || 'react_node_app';
+const user = process.env.DB_USER || 'admin';
+const password = process.env.DB_PASSWORD || 'pass12345';
+const database = process.env.DB_NAME || 'mysql';
 
 const db = mysql.createConnection({
    host: host,
